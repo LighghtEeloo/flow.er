5b782e15124ebff76b4a6bf48ba61f91df8660c5
LighghtEeloo <eelooecho@163.com>
2021-01-25 19:12:26 +0800

    - done: run. - done: icon.

---
+++ run.py
@@ -0,0 +1,18 @@
+import os
+from os.path import isdir, join
+from shutil import copytree, rmtree
+
+sync = [
+    'static',
+    'tracer.data'
+]
+
+des = 'dist'
+
+for x in sync:
+    dest = join(des, x)
+    if isdir(dest):
+        rmtree(dest)
+    copytree(x, dest)
+
+os.system('trunk serve')
