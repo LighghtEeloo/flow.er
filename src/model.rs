bd4a19f59ff44e61e1dffe0ba515abd67da56f55
LighghtEeloo <eelooecho@163.com>
2021-01-30 18:05:22 +0800

    - try: model & html.

--- src/model.rs
+++ src/model.rs
@@ -3,7 +3,6 @@
 
 use crate::prelude::*;
 
-use strum::IntoEnumIterator;
 use yew::format::Json;
 use yew::web_sys::HtmlInputElement as InputElement;
 use yew::{classes, html, Component, ComponentLink, Html, InputData, NodeRef, ShouldRender};
@@ -57,17 +56,31 @@ impl Component for Model {
     }
 
     fn view(&self) -> Html {
+        let list = vec!("a", "v");
         html! {
-            <div class="todomvc-wrapper">
-                <section class="todoapp">
+            <div class="app-wrapper">
+                <section class="sequence">
                     <header class="header">
-                        <h1>{ "todos" }</h1>
+                        <h1>{ "Sequence" }</h1>
                     </header>
+                </section>
+                <section class="main">
+                    <input
+                        type="checkbox"
+                        class="toggle-all"
+                        id="toggle-all"
+                        checked=true
+                        onclick=self.link.callback(|_| Msg::ToggleAll)
+                    />
+                    <label for="toggle-all" />
+                    <ul class="todo-list">
+                        { for list.iter().map(|e| html!(<div>{e}</div>)) }
+                    </ul>
                 </section>
                 <footer class="info">
                     <p>{ "Double-click to edit a todo" }</p>
-                    <p>{ "Written by " }<a href="https://github.com/DenisKolodin/" target="_blank">{ "Denis Kolodin" }</a></p>
-                    <p>{ "Part of " }<a href="http://todomvc.com/" target="_blank">{ "TodoMVC" }</a></p>
+                    <p>{ "Written by " }<a href="https://github.com/LighghtEeloo/" target="_blank">{ "LighghtEeloo" }</a></p>
+                    <p>{ "Inspired by " }<a href="https://github.com/DenisKolodin/" target="_blank">{ "Denis Kolodin" }</a></p>
                 </footer>
             </div>
         }
