4408ee08a8c419ea390700a210c7e534da250271
LighghtEeloo <eelooecho@163.com>
2021-02-05 22:34:02 +0800

    - reframe: view.

--- src/model.rs
+++ src/model.rs
@@ -1,6 +1,7 @@
 #[allow(dead_code)]
 #[allow(unused_assignments)]
 #[allow(unused)]
+mod view;
 mod editor;
 
 use crate::util::*;
@@ -158,43 +159,3 @@ impl Component for Model {
 }
 
 
-
-impl Model {
-    fn sidebar_tabs() -> Html {
-        let tab_meta: Vec<(&str, &str, bool)> = vec! {
-            ("static/icons/hexagons.svg", "Workspace", false),
-            ("static/icons/branch.svg", "Projects", false),
-            ("static/icons/history.svg", "History", false),
-            ("static/icons/settings.svg", "Settings", true),
-        };
-        let sidebar_tabs: Html = 
-            tab_meta.iter().map(
-                |(src, describe, bottom)| {
-                    html! {
-                        <li class={if !bottom {"tab"} else {"tab tab-bottom"}}>
-                            <div class="tab-content">
-                                <img src={&*src} alt={&*describe}/>
-                                <span class="tooltip">{&*describe}</span>
-                            </div>
-                        </li>
-                    }
-                }
-            ).collect();
-        html! {
-            { sidebar_tabs }
-        }
-    }
-
-    fn main_editor(&self) -> Html {
-        let view_new = html! {
-            <div class="cube-new">
-            { self.cube_new_input_view() }
-            </div>
-        };
-        let view_main = self.cube_view();
-
-        // Test: cube - new?
-        if self.cube.empty() && self.cube.name.is_empty() { view_new } else { view_main }
-    }
-
-}
---
+++ src/model/view.rs
@@ -0,0 +1,41 @@
+use crate::model::*;
+
+impl Model {
+    pub fn sidebar_tabs() -> Html {
+        let tab_meta: Vec<(&str, &str, bool)> = vec! {
+            ("static/icons/hexagons.svg", "Workspace", false),
+            ("static/icons/branch.svg", "Projects", false),
+            ("static/icons/history.svg", "History", false),
+            ("static/icons/settings.svg", "Settings", true),
+        };
+        let sidebar_tabs: Html = 
+            tab_meta.iter().map(
+                |(src, describe, bottom)| {
+                    html! {
+                        <li class={if !bottom {"tab"} else {"tab tab-bottom"}}>
+                            <div class="tab-content">
+                                <img src={&*src} alt={&*describe}/>
+                                <span class="tooltip">{&*describe}</span>
+                            </div>
+                        </li>
+                    }
+                }
+            ).collect();
+        html! {
+            { sidebar_tabs }
+        }
+    }
+
+    pub fn main_editor(&self) -> Html {
+        let view_new = html! {
+            <div class="cube-new">
+            { self.cube_new_input_view() }
+            </div>
+        };
+        let view_main = self.cube_view();
+
+        // Test: cube - new?
+        if self.cube.empty() && self.cube.name.is_empty() { view_new } else { view_main }
+    }
+
+}
