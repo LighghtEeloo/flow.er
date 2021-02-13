d9a4c382fee0d3148bcd62cf7fd17406bd366261
LighghtEeloo <2415467501@qq.com>
2021-02-14 02:00:57 +0800

    - reframe: status-bar.

--- src/ui/view.rs
+++ src/ui/view.rs
@@ -6,34 +6,15 @@ impl Model {
     pub fn main_view(&self) -> Html {
         html! {
             <div class="app-wrapper">
-                <div class="frame" id="left-sidebar">
-                    { self.sidebar_tabs() }
-                </div>
-                <div class="frame" id="main-editor">
-                    { self.main_editor() }
-                </div>
-                <div class="frame" id="status-bar">
-                    { self.src_view_button_view() }
-                    { self.export_button_view() }
-                    <p style="
-                        position: absolute;
-                        top: 0; bottom: 0; left: 0; right: 0;
-                        height: 100%;
-                        width: 60%;
-                        margin: auto;
-                        font-family: cursive;
-                        text-align: center;
-                        font-size: 8pt;
-                    ">
-                        {"Lorem ipsum dolor sit amet consectetur, adipisicing elit."}
-                    </p>
-                </div>
+                { self.sidebar_tabs() }
+                { self.main_editor() }
+                { self.status_bar() }
                 <script src="static/third/clip.js"/>
             </div>
         }
     }
 
-    pub fn sidebar_tabs(&self) -> Html {
+    fn sidebar_tabs(&self) -> Html {
         let tab_meta: Vec<(&str, &str, bool)> = vec! {
             ("static/icons/hexagons.svg", "Workspace", false),
             ("static/icons/branch.svg", "Projects", false),
@@ -54,21 +35,48 @@ impl Model {
                 }
             ).collect();
         html! {
-            { sidebar_tabs }
+            <div class="frame" id="left-sidebar">
+                { sidebar_tabs }
+            </div>
         }
     }
 
-    pub fn main_editor(&self) -> Html {
+    fn main_editor(&self) -> Html {
         // Fixme: cube - new?
-        if self.cube.empty() && self.cube.name.is_empty() { 
+        let editor = if self.cube.empty() && self.cube.name.is_empty() { 
             self.cube_new_view() 
         } else if self.src_view { 
             self.cube_src_view()
         } else { 
             self.cube_view() 
+        };
+        html! {
+            <div class="frame" id="main-editor">
+                { editor }
+            </div>
         }
     }
 
+    fn status_bar(&self) -> Html {
+        html! {
+            <div class="frame" id="status-bar">
+                { self.src_view_button_view() }
+                { self.export_button_view() }
+                <p style="
+                    position: absolute;
+                    top: 0; bottom: 0; left: 0; right: 0;
+                    height: 100%;
+                    width: 60%;
+                    margin: auto;
+                    font-family: cursive;
+                    text-align: center;
+                    font-size: 8pt;
+                ">
+                    {"Lorem ipsum dolor sit amet consectetur, adipisicing elit."}
+                </p>
+            </div>
+        }
+    }
 }
 
 
