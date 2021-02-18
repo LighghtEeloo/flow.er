8217902157508ae519e0dba838448ec2c5832fd7
LighghtEeloo <eelooecho@163.com>
2021-02-18 22:59:17 +0800

    - impl: TagSet.

--- src/stockpile/tag_set.rs
+++ src/stockpile/tag_set.rs
@@ -4,12 +4,54 @@ pub type Tag = String;
 
 #[derive(Default, Clone, Debug, Deserialize, Serialize)]
 pub struct TagSet {
-    pub data: Vec<Tag>,
+    data: Vec<Tag>,
 }
 
 impl TagSet {
     pub fn new() -> Self {
         Self::default()
     }
+    pub fn into_iter(&self) -> std::vec::IntoIter<Tag> {
+        self.data.clone().into_iter()
+    }
+    pub fn position(&self, tag: &Tag) -> Option<usize> {
+        self.into_iter().position(|x| x.clone() == tag.clone())
+    }
+    fn find(&self, tag: &Tag) -> bool {
+        self.position(&tag).is_some()
+    }
+    pub fn insert(&mut self, index: usize, tag: Tag) -> bool {
+        let inserting = !self.find(&tag) && index <= self.data.len();
+        if inserting {
+            self.data.insert(index, tag);
+        }
+        inserting
+    }
+    pub fn push(&mut self, tag: Tag) -> bool {
+        let inserting = !self.find(&tag);
+        if inserting {
+            self.data.push(tag);
+        }
+        inserting
+    }
+    pub fn remove(&mut self, tag: Tag) -> bool {
+        let removing = self.find(&tag);
+        self.data.retain(|x| x.clone() != tag);
+        removing
+    }
+
+
+    // pub fn iter(&self) -> Iter {
+    //     Iter(self.data.clone().into_iter().rev().collect())
+    // }
 }
 
+// /// Iter: 
+// pub struct Iter(Vec<Tag>);
+
+// impl Iterator for Iter {
+//     type Item = Tag;
+//     fn next(&mut self) -> Option<<Self as Iterator>::Item> { 
+//         self.0.pop()
+//     }
+// }
