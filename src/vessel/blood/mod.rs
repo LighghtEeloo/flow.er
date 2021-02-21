dfbb211f38f30c7d98f9399180cfe6b5d91f9836
LighghtEeloo <eelooecho@163.com>
2021-02-21 23:22:20 +0800

    - reframe: blood.

---
+++ src/vessel/blood/mod.rs
@@ -0,0 +1,14 @@
+mod flow;
+mod dancer;
+mod linear;
+mod graph;
+
+pub mod prelude {
+    pub use super::super::prelude::*;
+    pub use super::flow::{ Flow, FlowNode, FlowNodeNotFoundError };
+    pub use super::dancer::{ Direction, FixState, Dancer };
+    // pub use super::dancer::{ Direction, FixState, RelativePath, Dancer };
+    pub use super::linear::{ Linear };
+    pub use super::graph::{ Graph };
+    // pub use super::;
+}
--- src/vessel/mod.rs
+++ src/vessel/mod.rs
@@ -1,16 +1,8 @@
 mod salt;
-mod flow;
-mod dancer;
-mod linear;
-mod graph;
+mod blood;
 mod vessel;
 
 pub mod prelude {
     pub use super::salt::prelude::*;
-    pub use super::flow::{ Flow, FlowNode, FlowNodeNotFoundError };
-    pub use super::dancer::{ Direction, FixState, Dancer };
-    // pub use super::dancer::{ Direction, FixState, RelativePath, Dancer };
-    pub use super::linear::{ Linear };
-    pub use super::graph::{ Graph };
-    // pub use super::;
+    pub use super::blood::prelude::*;
 }
