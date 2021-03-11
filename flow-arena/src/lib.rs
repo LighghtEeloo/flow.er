9ea59343dbb1809b80dbb50751a0668fcc9bfe5d
LighghtEeloo <eelooecho@163.com>
2021-03-12 05:30:35 +0800

    - impl! serde.

--- flow-arena/src/lib.rs
+++ flow-arena/src/lib.rs
@@ -1,21 +1,11 @@
-use std::{collections::HashMap, fmt::Debug, hash::Hash};
-#[cfg(feature = "serde")]
+use std::{collections::HashMap, fmt::{self, Debug}, hash::Hash};
+
+#[cfg(feature = "serde1")]
 use serde::{Serialize, Deserialize};
 
-#[cfg(not(feature = "serde"))]
-#[derive(Default)]
-// Debug..
-#[derive(Clone, PartialEq)]
-pub struct Node<Id> {
-    id: Id,
-    parent: Option<Id>,
-    children: Vec<Id>,
-}
-#[cfg(feature = "serde")]
-#[derive(Default)]
-#[derive(Serialize, Deserialize)]
-// Debug..
-#[derive(Clone, PartialEq)]
+#[derive(Clone, Default)]
+#[cfg_attr(feature = "serde1", derive(Serialize, Deserialize))]
+#[cfg_attr(debug_assertions, derive(PartialEq))]
 pub struct Node<Id> {
     id: Id,
     parent: Option<Id>,
@@ -35,8 +25,9 @@ impl<Id> Node<Id> {
     }
 }
 
+#[cfg(debug_assertions)]
 impl<Id: Debug> Debug for Node<Id> {
-    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
         f.debug_struct(format!("{:?}", self.id()).as_str())
             .field("parent", &self.parent)
             .field("children", &self.children)
@@ -60,20 +51,12 @@ pub trait FlowLike {
     fn purge(&mut self, obj: &Self::Id) -> Result<(), ()>;
 }
 
-#[cfg(not(feature = "serde"))]
-// Debug..
-#[derive(Debug, Clone)]
-pub struct Flow<Id> {
-    /// root: can be a Nil node or a dummy node, but must be in node_map.
-    root: Id,
-    node_map: HashMap<Id, Node<Id>>,
-}
 
-#[cfg(feature = "serde")]
-#[derive(Serialize, Deserialize)]
-// Debug..
-#[derive(Debug, Clone)]
-pub struct Flow<Id> {
+// #[cfg_attr(feature = "serde1", derive(Serialize, Deserialize))]
+#[derive(Clone)]
+#[cfg_attr(debug_assertions, derive(PartialEq))]
+#[cfg_attr(debug_assertions, derive(Debug))]
+pub struct Flow<Id: Hash + Eq> {
     /// root: can be a Nil node or a dummy node, but must be in node_map.
     root: Id,
     node_map: HashMap<Id, Node<Id>>,
@@ -185,14 +168,122 @@ impl<Id: Clone + Hash + Eq + Default + Debug> FlowLike for Flow<Id> {
     }
 }
 
+#[cfg(feature = "serde1")]
+use serde::ser::{Serializer, SerializeStruct};
+#[cfg(feature = "serde1")]
+impl<Id: Serialize + Hash + Eq> Serialize for Flow<Id> {
+    fn serialize<S: Serializer>(&self, serializer: S) -> Result<S::Ok, S::Error> {
+        let mut flow = serializer.serialize_struct("Flow", 2)?;
+        flow.serialize_field("root", &self.root)?;
+        // let mut seq = serializer.serialize_seq(Some(self.node_map.len()))?;
+        // for (_, e) in self.node_map {
+        //     seq.serialize_element(e)?;
+        // }
+        let seq: Vec<&Node<Id>> = self.node_map.values().collect();
+        flow.serialize_field("node_map", &seq)?;
+        flow.end()
+    }
+}
+
+#[cfg(feature = "serde1")]
+use serde::de::{self, Deserializer, Visitor, SeqAccess, MapAccess};
+#[cfg(feature = "serde1")]
+use std::marker::PhantomData;
+#[cfg(feature = "serde1")]
+impl<'de, Id: Clone + Hash + Eq + Deserialize<'de>> Deserialize<'de> for Flow<Id> {
+    fn deserialize<D: Deserializer<'de>>(deserializer: D) -> Result<Self, D::Error> {
+        enum Field { Root, NodeMap }
+        impl<'de> Deserialize<'de> for Field {
+            fn deserialize<D: Deserializer<'de>>(deserializer: D) -> Result<Field, D::Error> {
+                struct FieldVisitor;
+
+                impl<'de> Visitor<'de> for FieldVisitor {
+                    type Value = Field;
+
+                    fn expecting(&self, formatter: &mut fmt::Formatter) -> fmt::Result {
+                        formatter.write_str("`root` or `node_map`")
+                    }
+
+                    fn visit_str<E>(self, value: &str) -> Result<Field, E>
+                    where
+                        E: de::Error,
+                    {
+                        match value {
+                            "root" => Ok(Field::Root),
+                            "node_map" => Ok(Field::NodeMap),
+                            _ => Err(de::Error::unknown_field(value, FIELDS)),
+                        }
+                    }
+                }
+
+                deserializer.deserialize_identifier(FieldVisitor)
+            }
+        }
+
+        struct FlowVisitor<Id: Hash + Eq> {
+            marker: PhantomData<fn() -> Flow<Id>>
+        }
+
+        impl<'de, Id: Clone + Hash + Eq + Deserialize<'de>> Visitor<'de> for FlowVisitor<Id> {
+            type Value = Flow<Id>;
+
+            fn expecting(&self, formatter: &mut fmt::Formatter) -> fmt::Result {
+                formatter.write_str("struct Flow")
+            }
+
+            fn visit_seq<V>(self, mut seq: V) -> Result<Self::Value, V::Error>
+            where
+                V: SeqAccess<'de>,
+            {
+                let root = seq.next_element()?
+                    .ok_or_else(|| de::Error::invalid_length(0, &self))?;
+                let node_vec: Vec<Node<Id>> = seq.next_element()?
+                    .ok_or_else(|| de::Error::invalid_length(1, &self))?;
+                let node_map = node_vec.into_iter().map(|node| (node.id.clone(), node)).collect();
+                Ok(Self::Value { root, node_map })
+            }
+
+            fn visit_map<V>(self, mut map: V) -> Result<Self::Value, V::Error>
+            where
+                V: MapAccess<'de>,
+            {
+                let mut root = None;
+                let mut node_map = None;
+                while let Some(key) = map.next_key()? {
+                    match key {
+                        Field::Root => {
+                            if root.is_some() {
+                                return Err(de::Error::duplicate_field("root"));
+                            }
+                            root = Some(map.next_value()?);
+                        }
+                        Field::NodeMap => {
+                            if node_map.is_some() {
+                                return Err(de::Error::duplicate_field("node_map"));
+                            }
+                            let node_vec: Vec<Node<Id>> = map.next_value()?;
+                            node_map = Some(node_vec.into_iter().map(|node| (node.id.clone(), node)).collect());
+                        }
+                    }
+                }
+                let root = root.ok_or_else(|| de::Error::missing_field("root"))?;
+                let node_map = node_map.ok_or_else(|| de::Error::missing_field("node_map"))?;
+                Ok(Self::Value { root, node_map })
+            }
+        }
+
+        const FIELDS: &'static [&'static str] = &["root", "node_map"];
+        deserializer.deserialize_struct("Flow", FIELDS, FlowVisitor { marker: PhantomData })
+    }
+}
 
 #[cfg(test)]
 mod tests {
     use super::*;
-    use std::fmt;
     type FlowEntity = Flow<EntityId>;
     type NodeEntity = Node<EntityId>;
     #[derive(Clone, Default, Hash, PartialEq, Eq)]
+    #[cfg_attr(feature = "serde1", derive(Serialize, Deserialize))]
     struct EntityId {
         idx: u64,
     }
@@ -207,32 +298,59 @@ mod tests {
         }
     }
 
-    #[test]
-    fn main_test() {
-        let wrapper = |name: &str, res: bool, flow: &FlowEntity| {
+    fn wrapper(name: &str, res: bool, flow: &FlowEntity, aloud: bool) {
+        if aloud {
             println!("{}: {}", name, if res {"success"} else {"error"});
             println!("{:#?}", flow);
-        };
+        }
+    }
+
+    fn make_flow(aloud: bool) -> FlowEntity {
         let mut flow: FlowEntity = Flow::new();
         let root = flow.root().id().clone();
         let obj_vec: Vec<NodeEntity> = (0..21).collect::<Vec<u64>>().iter_mut().map(|x| Node::from_id(x.clone().into())).collect();
-        println!("{:?}", obj_vec.last());
-        wrapper("Grow", flow.grow(obj_vec[1].clone()).is_ok(), &flow);
-        wrapper("Grow", flow.grow(obj_vec[2].clone()).is_ok(), &flow);
-        wrapper("Grow", flow.grow(obj_vec[3].clone()).is_ok(), &flow);
-        wrapper("Grow", flow.grow(obj_vec[4].clone()).is_ok(), &flow);
-        wrapper("Devote", flow.devote(obj_vec[1].id(), &root, 0).is_ok(), &flow);
-        wrapper("Devote", flow.devote(obj_vec[2].id(), &root, 0).is_ok(), &flow);
-        wrapper("DevotePush", flow.devote_push(obj_vec[3].id(), &root).is_ok(), &flow);
-        wrapper("Devote", flow.devote(obj_vec[4].id(), obj_vec[1].id(), 0).is_ok(), &flow);
-        wrapper("Purge", flow.purge(obj_vec[1].id()).is_ok(), &flow);
-        wrapper("Decay", flow.decay(obj_vec[1].id()).is_ok(), &flow);
-        if cfg!(debug_assertions) { println!("Checked."); flow.check() };
+        wrapper("Grow", flow.grow(obj_vec[1].clone()).is_ok(), &flow, aloud);
+        wrapper("Grow", flow.grow(obj_vec[2].clone()).is_ok(), &flow, aloud);
+        wrapper("Grow", flow.grow(obj_vec[3].clone()).is_ok(), &flow, aloud);
+        wrapper("Grow", flow.grow(obj_vec[4].clone()).is_ok(), &flow, aloud);
+        wrapper("Devote", flow.devote(obj_vec[1].id(), &root, 0).is_ok(), &flow, aloud);
+        wrapper("Devote", flow.devote(obj_vec[2].id(), &root, 0).is_ok(), &flow, aloud);
+        wrapper("DevotePush", flow.devote_push(obj_vec[3].id(), &root).is_ok(), &flow, aloud);
+        wrapper("Devote", flow.devote(obj_vec[4].id(), obj_vec[1].id(), 0).is_ok(), &flow, aloud);
+        wrapper("Purge", flow.purge(obj_vec[1].id()).is_ok(), &flow, aloud);
+        wrapper("Decay", flow.decay(obj_vec[1].id()).is_ok(), &flow, aloud);
+        if cfg!(debug_assertions) && aloud { println!("Checked."); flow.check() };
+        flow
+    }
+
+    #[test]
+    fn main_test() {
+        make_flow(true);
     }
 
     #[test]
     fn root() {
         let mut flow: FlowEntity = Flow::new();
         assert_eq!(flow.root().clone(), Node::default());
+    }
+
+    #[test]
+    fn serde() {
+        let print_wrapper = |str: &String, aloud: bool| {
+            if aloud {
+                println!("{}",str)
+            }
+        };
+        let id: EntityId = 1.into();
+        print_wrapper(&serde_json::to_string(&id).unwrap(), false);
+        let node: NodeEntity = Node::from_id(1.into());
+        print_wrapper(&serde_json::to_string(&node).unwrap(), false);
+        let flow = make_flow(false);
+        // println!("{:#?}", flow);
+        let str = serde_json::to_string(&flow).unwrap();
+        print_wrapper(&str, false);
+        let _flow: FlowEntity = serde_json::from_str(&str).unwrap();
+        // println!("{:#?}", _flow);
+        assert_eq!(flow, _flow)
     }
 }
