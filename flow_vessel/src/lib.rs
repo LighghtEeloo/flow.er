9d9dcaede4eedd631d3247446a387d6f79b48779
LighghtEeloo <eelooecho@163.com>
2021-03-20 03:59:31 +0800

    - chore: vessel lib exports.

--- flow_vessel/src/lib.rs
+++ flow_vessel/src/lib.rs
@@ -1,14 +1,15 @@
 mod identity;
-mod entity;
 mod time;
 mod process;
 mod tag;
+mod entity;
 mod saveload;
 mod vessel;
 
 pub use flow_arena::{Flow, FlowArena, Node};
 pub use identity::{EntityId, EntityIdFactory};
-pub use entity::{Entity, EntityField, Face, Bubble};
 pub use time::{TimeNote, TimeRep};
+pub use process::ProcessStatus;
 pub use tag::{Tag, TagSet, TagSetField};
+pub use entity::{Entity, EntityField, Face, Bubble};
 pub use vessel::Vessel;
