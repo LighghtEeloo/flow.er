cfa15fcb5d35a03109c79a9ef41fb4b9d3bbabf1
LighghtEeloo <eelooecho@163.com>
2021-04-03 23:09:28 +0800

    - rapid! drafts the new flow_arena traits. - polish! graph part of the flow_arena.

--- flow_arena/src/flow.rs
+++ flow_arena/src/flow.rs
@@ -30,7 +30,7 @@ use std::hash::Hash;
 /// 
 /// To sum up, all the node "registration" processes are done via mounting and un-mounting to root.
 /// 
-/// C. Devote & Decay
+/// C. Tree Ability: Devote & Decay
 /// 
 /// devote: mounts an orphaned node to a non-root node, and unmounts it from root.
 /// 
@@ -38,11 +38,19 @@ use std::hash::Hash;
 /// 
 /// With the devote and decay functions, now the flow can express a tree structure.
 /// 
-/// D. Link
+/// D. Graph Ability
+/// 
+/// D.1. Link & Detach
 /// 
 /// link: randomly mounts an *unorphaned* node under a non-root node; the parent of the node will not be changed.
 /// 
-/// With link, now the flow can finally express a semi-graph structure with restrictions.
+/// detach: unmounts an *unorphaned* node from a non-root node; the parent of the node must still own the node.
+/// 
+/// With link and detach, now the flow can finally express a semi-graph structure with restrictions.
+/// 
+/// D.2. Defect
+/// 
+/// defect: switches an *unorphaned* node's owner to be another non-root node which already has access to the node.
 /// 
 pub trait Flow {
     type Id: Default + Hash + Eq + Clone;
@@ -73,5 +81,45 @@ pub trait Flow {
     /// removes a node mounted to the root and unmounts it; returns err if id not found under root
     fn erase(&mut self, obj: &Self::Id) -> Result<(), ()>;
 }
+
+pub trait FlowMap {
+    type Id: Default + Hash + Eq + Clone;
+    type Node: Default;
+    /// ensures root and returns it; no check
+    fn root(&mut self) -> &mut Self::Node;
+    /// no check
+    fn node(&self, obj: &Self::Id) -> Option<&Self::Node>;
+    /// inserts a node and mounts it to the root; returns err if id exists.
+    fn grow(&mut self, obj: Self::Node) -> Result<(), ()>;
+    /// removes a node mounted to the root and unmounts it; returns err if id not found under root
+    fn erase(&mut self, obj: &Self::Id) -> Result<(), ()>;
+}
+
+
+pub trait FlowTree: FlowMap {
+    /// mounts an orphaned node to a non-root node as the nth child, and unmounts it from root
+    ///
+    /// err if:
+    /// 1. obj not mounted under root 
+    /// 2. owner is root 
+    /// 3. nth > len
+    /// 4. no obj / owner
+    fn devote(&mut self, obj: &Self::Id, owner: &Self::Id, nth: usize) -> Result<(), ()>;
+    fn devote_push(&mut self, obj: &Self::Id, owner: &Self::Id) -> Result<(), ()>;
+    /// deletes all the links of a node and mounts it only to root
+    fn decay(&mut self, obj: &Self::Id) -> Result<(), ()>;
+}
+
+
+pub trait FlowGraph: FlowMap {
+    /// randomly mounts an *unorphaned* node under a non-root node; the parent of the node will not be changed
+    fn link(&mut self, obj: &Self::Id, owner: &Self::Id, nth: usize) -> Result<(), ()>;
+    fn link_push(&mut self, obj: &Self::Id, owner: &Self::Id) -> Result<(), ()>;
+    /// unmounts an *unorphaned* node from a non-root node; the parent of the node must still own the node
+    fn detach(&self, obj: &Self::Id) -> Result<(), ()>;
+    /// switches an *unorphaned* node's owner to be another non-root node which already has access to the node
+    fn defect(&mut self, obj: &Self::Id, owner: &Self::Id) -> Result<(), ()>;
+}
+
 
 
