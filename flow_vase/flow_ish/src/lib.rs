282d8269733e007ad6feeb51a4ee18847551708d
LighghtEeloo <eelooecho@163.com>
2021-04-22 19:53:49 +0800

    - init@flow-ish.

---
+++ flow_vase/flow_ish/src/lib.rs
@@ -0,0 +1,11 @@
+pub fn main() {
+}
+
+
+#[cfg(test)]
+mod tests {
+    #[test]
+    fn main() {
+        super::main()
+    }
+}
---
+++ flow_vase/flow_ish/src/main.rs
@@ -0,0 +1,6 @@
+//! flow-ish is a terminal app designed for flow.
+//! It has all the subcommands of flow-cli, but with a responsive user interface.
+
+fn main() {
+    flow_ish::main()
+}
