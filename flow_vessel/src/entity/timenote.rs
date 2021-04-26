7f560ca70ea4420eb4c8d5349479224e31ee7140
LighghtEeloo <eelooecho@163.com>
2021-04-27 02:42:49 +0800

    - done! timenote with tag. - done: timenote with end guard.

--- flow_vessel/src/entity/timenote.rs
+++ flow_vessel/src/entity/timenote.rs
@@ -1,31 +1,38 @@
 use chrono::{DateTime, Local};
 use serde::{Deserialize, Serialize};
-use std::{
-    fmt::Debug,
-    time::{Duration, SystemTime},
-};
+use std::{fmt::{self, Debug}, time::{Duration, SystemTime}};
 
-use crate::TimeRep;
+use crate::{Tag, TimeRep};
 
 #[derive(Default, Clone, Serialize, Deserialize)]
 pub struct TimeNote {
-    notes: Vec<SystemTime>,
+    notes: Vec<(SystemTime, Tag)>,
 }
 
 impl TimeNote {
     pub fn new() -> TimeNote {
         TimeNote::default()
     }
-    pub fn set_start(&mut self, start: SystemTime) -> &mut Self {
-        self.notes.insert(0, start);
+    pub fn add_start(&mut self, start: SystemTime) -> &mut Self {
+        self.notes.insert(0, (start, Tag::default()));
         self
     }
-    pub fn set_end(&mut self, end: SystemTime) -> &mut Self {
-        self.notes.push(end);
+    pub fn add_end(&mut self, end: SystemTime) -> &mut Self {
+        self.notes.push((end, Tag::default()));
         self
+    }
+    pub fn start_t(&self) -> Option<&SystemTime> {
+        self.notes.first().map(|note| &note.0)
+    }
+    pub fn end_t(&self) -> Option<&SystemTime> {
+        if self.notes.len() == 1 {
+            None
+        } else {
+            self.notes.last().map(|note| &note.0)
+        }
     }
     pub fn length(&self) -> Duration {
-        match (self.notes.first(), self.notes.last()) {
+        match (self.start_t(), self.end_t()) {
             (Some(s), Some(e)) => {
                 e.duration_since(s.clone()).unwrap_or_default()
             }
@@ -37,26 +44,26 @@ impl TimeNote {
 impl TimeRep for TimeNote {
     fn human_local_detail(
         &self,
-        f: &mut std::fmt::Formatter<'_>,
-    ) -> std::fmt::Result {
+        f: &mut fmt::Formatter<'_>,
+    ) -> fmt::Result {
         write!(f, "[:")?;
-        self.notes.first().map(|t| t.human_local_detail(f));
+        self.start_t().map(|t| t.human_local_detail(f));
         write!(f, " ~ ")?;
-        self.notes.last().map(|t| t.human_local_detail(f));
+        self.end_t().map(|t| t.human_local_detail(f));
         write!(f, ":]")?;
         write!(f, "")
     }
 
-    fn human_local(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+    fn human_local(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
         write!(f, "[:")?;
-        self.notes.first().map(|t| t.human_local(f));
+        self.start_t().map(|t| t.human_local(f));
         write!(f, " ~ ")?;
-        self.notes.last().map(|t| t.human_local(f));
+        self.end_t().map(|t| t.human_local(f));
         write!(f, ":]")?;
         write!(f, "")
     }
-    fn clock_local(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
-        if let Some(time) = None.or(self.notes.first()).or(self.notes.last()) {
+    fn clock_local(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        if let Some(time) = None.or(self.start_t()).or(self.end_t()) {
             let dt: DateTime<Local> = time.clone().into();
             write!(f, "{}", dt.format("%H:%M:%S").to_string())
         } else {
@@ -64,18 +71,18 @@ impl TimeRep for TimeNote {
         }
     }
 
-    fn human_utc(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+    fn human_utc(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
         write!(f, "[:")?;
-        self.notes.first().map(|t| t.human_utc(f));
+        self.start_t().map(|t| t.human_utc(f));
         write!(f, " ~ ")?;
-        self.notes.last().map(|t| t.human_utc(f));
+        self.end_t().map(|t| t.human_utc(f));
         write!(f, ":]")?;
         write!(f, "")
     }
 }
 
 impl Debug for TimeNote {
-    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
         if f.alternate() {
             self.human_local_detail(f)
         } else {
@@ -92,9 +99,9 @@ mod tests {
     fn time_log() {
         let u_dur = 200;
         let mut time = TimeNote::new();
-        time.set_start(SystemTime::now());
+        time.add_start(SystemTime::now());
         sleep(Duration::from_millis(u_dur));
-        time.set_end(SystemTime::now());
+        time.add_end(SystemTime::now());
         let dur = time.length();
         println!("{:?}", dur);
         assert!(dur > Duration::from_millis(u_dur));
